05547acad64387d5dbee4eb15c7fac3b221d37a1
Angela Santos <145032472+ASPOLAKA@users.noreply.github.com>
2024-03-21 14:04:28 -0300

    Update ex011.js

--- ex011.js
+++ ex011.js
@@ -1,7 +1,7 @@
-var idade = 15
+var idade = 70
 if (idade < 16) {
     console.log('Não vota!')
-} else  if (idade >= 16 && idade < 18){
+} else  if (idade >= 16 && idade < 18 || idade > 65){
         console.log('Voto opcional!')
 } else if ( idade >= 18) {
     console.log('Voto obrigatório!')

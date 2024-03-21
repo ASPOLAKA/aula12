3ffba52e4a83bd7b1aa7c244a45852f13cf3dd10
Angela Santos <145032472+ASPOLAKA@users.noreply.github.com>
2024-03-21 14:02:18 -0300

    Identificador de idade

--- ex011.js
+++ ex011.js
@@ -1,6 +1,8 @@
-var idade = 12
-if (idade < 18) {
-    console.log('Menor de idade')
-}else {
-    console.log('Maior de idade')
+var idade = 15
+if (idade < 16) {
+    console.log('Não vota!')
+} else  if (idade >= 16 && idade < 18){
+        console.log('Voto opcional!')
+} else if ( idade >= 18) {
+    console.log('Voto obrigatório!')
 }
